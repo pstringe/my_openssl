270ec5b65247c61e8cb3b939090e805cc68d67a9
Poitier Stringer <pstringe@e1z3r5p12.42.us.org>
2018-03-03 11:38:52 -0800

    wrote client, still need to write server

--- DevOps/herc/client/client.c
+++ DevOps/herc/client/client.c
@@ -6,7 +6,7 @@
 /*   By: pstringe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/02 15:52:32 by pstringe          #+#    #+#             */
-/*   Updated: 2018/03/03 11:12:17 by pstringe         ###   ########.fr       */
+/*   Updated: 2018/03/03 11:36:18 by pstringe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,43 +17,31 @@
 
 int main(int argc, char const **argv)
 {
-	int 				port;
+	int					con;
+	int					port;
 	int					sock;
-	int					ret;
+	char				resp[256];
 	struct sockaddr_in 	serv_addr;
-	//struct sockaddr_in 	addr;
-	char const			*msg;
-	char				buf[1024];
 
-	if (argc != 2)
+	if (argc < 2)
 	{
-		ft_putendl("please enter the poert of the server you wish to message, and then the message.");
+		ft_putendl("Please specify port");
 		return (-1);
 	}
-	msg = argv[1];
-	port = ft_atoi(argv[2]);
-	if ((sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP) < 0))
-	{
-		ft_putendl("socket error");
-		return (-1);
-	}
-	ft_memset(&serv_addr, '0', sizeof(serv_addr));
+	sock = socket(AF_INET, SOCK_STREAM, 0);
 	serv_addr.sin_family = AF_INET;
+	port = ft_atoi(argv[1]);
 	serv_addr.sin_port = htons(port);
-	if(inet_pton(AF_INET, argv[1], &serv_addr.sin_addr) <= 0)
+	//I can connect to any port on my local machine using 0.0.0.0 (INADDR_ANY)
+	serv_addr.sin_addr.s_addr = INADDR_ANY;
+	if (-1 == (con = (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)))))
 	{
-		ft_putendl("invalid address");
+		ft_putendl("Connection error");
 		return (-1);
 	}
-	if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
-	{
-		ft_putendl("failed to connect");
-		return (-1);
-	}
-	send(sock, msg, ft_strlen(msg), 0);
-	ft_putendl("message sent");
-	ret = read(sock, buf, 1024);
-	buf[ret] = '\0';
-	ft_putendl(buf);
+	recv(sock, &resp, sizeof(resp), 0);
+	ft_putendl("server response:");
+	ft_putendl(resp);
 	close(sock);
+	return (0);
 }
