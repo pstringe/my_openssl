06c825d308107809bd883408ee1b42f4cce2c43a
Poitier Stringer <pstringe@e1z3r5p12.42.us.org>
2018-03-03 20:22:12 -0800

    conditional responses work

--- DevOps/herc/client/client.c
+++ DevOps/herc/client/client.c
@@ -6,42 +6,44 @@
 /*   By: pstringe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/02 15:52:32 by pstringe          #+#    #+#             */
-/*   Updated: 2018/03/03 11:36:18 by pstringe         ###   ########.fr       */
+/*   Updated: 2018/03/03 19:59:36 by pstringe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include "libft.h"
+#define BUFF_SIZE 256
 
 int main(int argc, char const **argv)
 {
 	int					con;
 	int					port;
 	int					sock;
-	char				resp[256];
+	char				buf[BUFF_SIZE];
 	struct sockaddr_in 	serv_addr;
 
-	if (argc < 2)
+	if (argc < 3)
 	{
-		ft_putendl("Please specify port");
+		perror("Please specify port and msg");
 		return (-1);
 	}
 	sock = socket(AF_INET, SOCK_STREAM, 0);
 	serv_addr.sin_family = AF_INET;
 	port = ft_atoi(argv[1]);
 	serv_addr.sin_port = htons(port);
-	//I can connect to any port on my local machine using 0.0.0.0 (INADDR_ANY)
 	serv_addr.sin_addr.s_addr = INADDR_ANY;
-	if (-1 == (con = (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)))))
+	con = connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+	if (con < 0)
 	{
-		ft_putendl("Connection error");
+		perror("Connection error");
 		return (-1);
 	}
-	recv(sock, &resp, sizeof(resp), 0);
-	ft_putendl("server response:");
-	ft_putendl(resp);
+	write(sock, argv[2], ft_strlen(argv[2]));
+	read(sock, &buf, BUFF_SIZE);
+	ft_putendl(buf);
 	close(sock);
 	return (0);
 }
--- DevOps/herc/hydra/hydra.c
+++ DevOps/herc/hydra/hydra.c
@@ -6,66 +6,76 @@
 /*   By: pstringe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/02 17:52:13 by pstringe          #+#    #+#             */
-/*   Updated: 2018/03/02 18:39:07 by pstringe         ###   ########.fr       */
+/*   Updated: 2018/03/03 20:19:28 by pstringe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#define BUFF_SIZE 256
 
 int		main(int argc, char **argv)
 {
+	int					listening;
 	int					port;
-	int 				server_fd;
-	int 				new_socket;
 	int 				ret;
-	int					opt;
-	int					addrlen;
-	char				buf[1024];
-	char 				*pong;
-	struct sockaddr_in 	address;
+	int					s_sock;
+	int					c_sock;
+	char 				*msg;
+	char 				buf[BUFF_SIZE];
+	struct sockaddr_in	addr;
+	socklen_t			addr_len;
 
-	if (argc < 2)
+	if (argc != 2)
 	{
-		ft_putendl("Please specify port to listen on!");
+		perror("please specify port to listen on");
+		return (-1);
+	}
+	if (!(s_sock = socket(AF_INET, SOCK_STREAM, 0)))
+	{
+		perror("failed to get socket descriptor");
 		return (-1);
 	}
 	port = ft_atoi(argv[1]);
-	opt = 1;
-	addrlen = sizeof(address);
-	pong = "pong";
-	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+	addr.sin_family = AF_INET;
+	addr.sin_addr.s_addr = INADDR_ANY;
+	addr.sin_port = htons(port);
+	addr_len = sizeof(addr);
+	if (bind(s_sock, (struct sockaddr*)&addr, (socklen_t)sizeof(addr)) < 0)
 	{
-		ft_putendl("failed to create socket");
-		exit(EXIT_FAILURE);
+		perror("faild to bind port to address");
+		return (-1);
 	}
-	if(setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
+	if (listen(s_sock, 5) == -1)
 	{
-		ft_putendl("error attatching socket to port");
-		exit(EXIT_FAILURE);
+		perror("server is not listening");
 	}
-	address.sin_family = AF_INET;
-	address.sin_addr.s_addr = INADDR_ANY;
-	address.sin_port = htons(port);
-	if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
+	listening = 1;
+	while (listening)
 	{
-		ft_putendl("failed to bind");
-		exit(EXIT_FAILURE);
+		c_sock = accept(s_sock, (struct sockaddr*)&addr, (socklen_t*)&addr_len);
+		if (c_sock < 0)
+		{
+			perror("did not accept");
+		}
+		ft_bzero(buf, BUFF_SIZE);
+		ret = read(c_sock, &buf, BUFF_SIZE);
+		ft_putendl(buf);
+		if (!ft_strncmp(buf, "ping", ft_strlen(buf)))
+			msg = "pong pong";
+		else if (!ft_strncmp(buf, "quit", ft_strlen(buf)))
+		{
+			listening = 0;
+			msg = "quit";
+		}
+		else
+		{
+			msg = "command not recongnized";
+		}
+		write(c_sock, msg, ft_strlen(msg));
+		close(c_sock);
 	}
-	if (listen(server_fd, 3) < 0)
-	{
-		ft_putendl("I'm not listening!");
-		exit(EXIT_FAILURE);
-	}
-	if ((new_socket = accept(server_fd, (struct sockaddr *)(&address), (socklen_t*)(&addrlen))) < 0)
-	{
-		ft_putendl("I do not accept.");
-		exit(EXIT_FAILURE);
-	}
-	ret = read(new_socket, buf, 1024);
-	buf[ret] = '\0';
-	ft_putendl(buf);
-	send(new_socket, pong, ft_strlen(pong), 0);
-	ft_putendl("pong sent");
+	return(0);
 }
