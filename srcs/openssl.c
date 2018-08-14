97c6152dcf84f32f26c3f92761ca77d5e1be14b8
Poitier Stringer <pstringe@e1z2r3p17.42.us.org>
2018-08-14 11:09:13 -0700

    able to take msg_text as argument

--- srcs/openssl.c
+++ srcs/openssl.c
@@ -6,7 +6,7 @@
 /*   By: pstringe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/13 07:11:57 by pstringe          #+#    #+#             */
-/*   Updated: 2018/08/14 09:21:35 by pstringe         ###   ########.fr       */
+/*   Updated: 2018/08/14 11:08:50 by pstringe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ t_args	*init_ssl()
 	t_args	*args;
 
 	args = ft_memalloc(sizeof(t_args));
-	args->arg = NULL;
+	args->msg = NULL;
 	args->cmd = NULL;
 	args->ops = 0;
 	return (args);
@@ -50,7 +50,7 @@ int 	get_ops(t_args *args, char *str)
 **	parse program arguments
 */
 
-void	parse(t_args *args, int argc, char **argv)
+int		parse(t_args *args, int argc, char **argv)
 {
 	int i;
 	int j;
@@ -62,9 +62,10 @@ void	parse(t_args *args, int argc, char **argv)
 		while (++j < NO_OF_FUNCS && !args->cmd)
 			if (!ft_strncmp(g_cmd[j].name, argv[i], ft_strlen(argv[i])) && i++)
 				args->cmd = &g_cmd[j];
-		while (i < NO_OF_FUNCS && argv[i][0] == '-' && !get_ops(args, argv[i]))
+		while (i < argc && argv[i][0] == '-' && !get_ops(args, argv[i]))
 			i++;
 	}
+	return (--i);
 }
 
 /*
@@ -73,7 +74,7 @@ void	parse(t_args *args, int argc, char **argv)
 
 void	md5(t_args *args)
 {
-	ft_printf("md5 trigger: %s\n", args->cmd->name);
+	ft_printf("md5 trigger: %s\nmessage: %s", args->cmd->name, args->msg);
 }
 
 /*
@@ -82,7 +83,17 @@ void	md5(t_args *args)
 
 void	sha(t_args *args)
 {
-	ft_printf("sha-256 trigger: %s\n", args->cmd->name);
+	ft_printf("sha-256 trigger: %s\nmessage: %s", args->cmd->name, args->msg);
+}
+
+/*
+**	retrieve the plain-text input
+*/
+
+void	get_msg(t_args *args, int idx, char **argv, int argc)
+{
+	if (idx < argc && argv[idx][0] != '-')
+		args->msg = ft_strdup(argv[idx]);
 }
 
 int		main(int argc, char **argv)
@@ -90,7 +101,6 @@ int		main(int argc, char **argv)
 	t_args	*args;	
 	
 	args = init_ssl();
-	parse(args, argc, argv);
-	//get_msg(args);
+	get_msg(args, parse(args, argc, argv), argv, argc);
 	args->cmd->func(args);
 }
