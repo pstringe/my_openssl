3d1ad13647dab741559a7e220f570712edff133c
Poitier Stringer <pstringe@e1z2r4p27.42.us.org>
2018-08-17 09:18:40 -0700

    cleaned up tests and padding function

--- srcs/openssl.c
+++ srcs/openssl.c
@@ -6,7 +6,7 @@
 /*   By: pstringe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/13 07:11:57 by pstringe          #+#    #+#             */
-/*   Updated: 2018/08/16 19:36:50 by pstringe         ###   ########.fr       */
+/*   Updated: 2018/08/17 09:17:57 by pstringe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -122,130 +122,83 @@ void	get_msg(t_args *args, int idx, char **argv, int argc)
 		ft_printf("no argument\n");
 }
 
-void	print_block(char block[64])
+void	print_block(t_args *args)
 {
-	int i;
-	int j;
-	int k;
+	int 	i;
+	int		j;
+	char 	*block;
 	
-	i = 0;
-	while (++i <= 4)
+	i = -1;
+	while ((block = (char*)ft_dequeue(args->blocks)))
 	{
-		j = 0;
-		while (++j <= 4)
-		{
-			k = 0;
-			while (++k <= 4)
-				ft_putchar(block[(i % j) * (j % k)]);
-			ft_putchar(' ');
-		}
+		ft_printf("block %d:\n", ++i);
+		j = -1;
+		while (++j < 65)
+			ft_putchar(block[j] < 41 ? '.' : block[j]);
 		ft_putchar('\n');
 	}
 }
 
-t_queue		*ft_queuenw(void *n, size_t size)
-{
-	t_queue	*q;
-	t_list	*tmp;
-
-	q = (t_queue*)ft_memalloc(sizeof(t_queue));
-	tmp = ft_lstnew(n, size);
-	q->tail = tmp;
-	q->head = tmp;
-	return (q);
-}
-
-void		*ft_dequeue(struct s_queue *q)
-{
-	t_list		*tmp;
-	void		*t;
-
-	if (!q->head)
-		return (NULL);
-	tmp = q->head;
-	q->head = tmp->next;
-	t = ft_memalloc(tmp->content_size);
-	ft_memcpy(t, tmp->content, tmp->content_size);
-	ft_memdel((void**)&(tmp->content));
-	ft_memdel((void**)&tmp);
-	if (!q->head)
-		q->tail = NULL;
-	return (t);
-}
 /*
 **	split the message into 512bit (64 byte blocks) and pad up to 48bits
 **	with the last 64 bits representing the length of the original message
 */
 
+	//establish length of original message
+	//establish the expected number of complete blocks which do not require padding
+	//establish the remaining number of characters after the last complete block which must be padded into a complete block
+	//this loop will execute, if and only if there are enough characters to form at least one complete block.
+	//copy 64 bytes from the msg text to a block
+	//enque a copy of this block
+	//increment the index by the number of copied bytes
+	//zero out the block
+	//copy the remaining number of bytes in the msg text over to bloc
+	//increment i by the remaining number of bytes
+	//set j to the remaining no of bytes
+	//I append a bit to the end of the msg text by ORing it against that index
+	//I pad the block with null characters until the index is 64bits shy of a complete block
+	//I use the last 64 bits to represent the original length of the msg
+	//ft_bzero(&block[j], 8);
+	//for each of the remaining 8 bytes, on the block,
+		/* 
+		 * isolate the appropriate bits from the total length starting with the most significant,
+		 * shift them to the least significant place value, and OR them against the current byte
+		 *  in the block
+		 */
+	//initialize the queue in the event there was <= 1 blocks worth of text
 void	get_blocks(t_args *args)
 {
 	uint64_t	len;
 	int 		blocks;
 	int	 		remainder;
 	char		block[64];
-	int			i; //position in the original message
-	int 		j; //position in padding block
-	int			k; //bit mask
-	int			l; //shift the result of the bit mask to least significant place-value
+	t_pad		p;
 
-	//establish length of original message
 	len = ft_strlen(args->msg);
-
-	//establish the expected number of complete blocks which do not require padding
 	blocks = len / 64;
-
-	//establish the remaining number of characters after the last complete block which must be padded into a complete block
 	remainder = len % 64;
-	i = 0;
-
-	//this loop will execute, if and only if there are enough characters to form at least one complete block.
+	p.msg_idx = 0;
 	while (blocks-- > 0)
 	{
-		//copy 64 bytes from the msg text to a block
-		ft_memcpy(block, args->msg + i, 64);
-		//enque a copy of this block
+		ft_memcpy(block, args->msg + p.msg_idx, 64);
 		if (!args->blocks)
 			args->blocks = ft_queuenw(ft_strdup(block), 64);
 		else
 			ft_enqueue(args->blocks, ft_strdup(block), 64);
-		//increment the index by the number of copied bytes
-		i += 64;
+		p.msg_idx += 64;
 	}
-	//zero out the block
 	ft_bzero(block, 64);
+	ft_memcpy(block, args->msg + p.msg_idx, len % 64);
+	p.msg_idx += len % 64;
+	p.blk_idx = len % 64;
+	block[p.blk_idx] |= (1 << 7);
 
-	//copy the remaining number of bytes in the msg text over to bloc
-	ft_memcpy(block, args->msg + i, len % 64);
-	
-	//increment i by the remaining number of bytes
-	i += len % 64;
-
-	//set j to the remaining no of bytes
-	j = len % 64;
-
-	//I append a bit to the end of the msg text by ORing it against that index
-	block[j] |= (1 << 7);
-
-	//I pad the block with null characters until the index is 64bits shy of a complete block
-	while (++j < 64 - 8)
-		block[j] = '\0';
-
-	//I use the last 64 bits to represent the original length of the msg
-	//ft_bzero(&block[j], 8);
-	
-	k = 8; //bit mask shift
-	l = 8; //shift on the resulting bit mask to the least significant place value
-	
-	//for each of the remaining 8 bytes, on the block,
-	while(j < 64 && k >= 0)
-		/* 
-		 * isolate the appropriate bits from the total length starting with the most significant,
-		 * shift them to the least significant place value, and OR them against the current byte
-		 *  in the block
-		 */
-		block[j++] |= (((0x00000000000000ff << ((--k) * 8)) & len) >> (--l) * 8);
-	
-	//initialize the queue in the event there was <= 1 blocks worth of text
+	while (++p.blk_idx < 64 - 8)
+		block[p.blk_idx] = '\0';
+	p.bit_shft = 8;
+	p.nrm_shft = 8;
+	while(p.blk_idx < 64 && p.bit_shft >= 0)
+		block[p.blk_idx++] |= (((0x00000000000000ff << ((--p.bit_shft) * 8)) & len) >> (--p.nrm_shft) * 8);
 	if (!args->blocks)
 		args->blocks = ft_queuenw(ft_strdup(block), 64 + 1);
 	else
@@ -259,17 +212,6 @@ int		main(int argc, char **argv)
 	args = init_ssl();
 	get_msg(args, parse(args, argc, argv), argv, argc);
 	get_blocks(args);
-	/*test*/
-	int 	i = -1;
-	char 	*block;
-	while ((block = (char*)ft_dequeue(args->blocks)))
-	{
-		ft_printf("block %d:\n", ++i);
-		int j = -1;
-		while (++j < 65)
-			ft_putchar(block[j] < 41 ? '.' : block[j]);
-		ft_putchar('\n');
-	}
-	/*end test*/
+	print_block(args);
 	args->cmd->func(args);
 }
